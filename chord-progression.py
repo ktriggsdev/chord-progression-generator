fc5147e523f5c967ed687cce65f20340279e4e97
Kieran Triggs <127336461+ktriggsdev@users.noreply.github.com>
2023-04-16 12:45:49 +0100

    Update chord-progression.py

--- chord-progression.py
+++ chord-progression.py
@@ -2,81 +2,39 @@ import streamlit as st
 import numpy as np
 import random
 
-# Define a function to generate a random chord progression
-def generate_chord_progression(key, num_chords):
-  chords = ["C", "Dm", "Em", "F", "G", "Am", "Bb", "C"]
-  progression = []
-  for i in range(num_chords):
-    progression.append(random.choice(chords))
-  return progression
+# Define the chord progressions
+chord_progressions = [
+    ["C", "G", "Am", "F"],
+    ["Dm", "G", "C", "Am"],
+    ["Em", "A", "D", "G"],
+    ["F", "C", "G", "Am"],
+]
 
-# Create a function to play the chord progression
-def play_chord_progression(progression, key):
-  # Create a list of frequencies for each chord
-  frequencies = []
-  for chord in progression:
-    frequencies.append(get_frequencies(chord, key))
+# Define the function to generate a random chord progression
+def generate_chord_progression():
+    # Get a random chord progression from the list
+    progression = random.choice(chord_progressions)
 
-  # Create a sound object
-  sound = np.array(frequencies).astype("float32")
+    # Convert the chord progression to a list of notes
+    notes = []
+    for chord in progression:
+        notes.extend(np.array(chord))
 
-  # Play the sound
-  st.audio(sound)
+    # Return the list of notes
+    return notes
 
-# Define a function to get the frequencies for a chord
-def get_frequencies(chord, key):
-  # Get the notes in the chord
-  notes = get_notes(chord)
-
-  # Get the frequencies for each note
-  frequencies = []
-  for note in notes:
-    frequencies.append(get_frequency(note, key))
-
-  return frequencies
-
-# Define a function to get the notes in a chord
-def get_notes(chord):
-  # Create a dictionary that maps chords to notes
-  chords_to_notes = {
-    "C": ["C", "E", "G"],
-    "Dm": ["D", "F", "A"],
-    "Em": ["E", "G", B],
-    "F": ["F", A, C],
-    "G": ["G", B, D],
-    "Am": ["A", C, E],
-    "Bb": ["Bb", D, F],
-  }
-
-  # Get the notes for the specified chord
-  notes = chords_to_notes[chord]
-
-  return notes
-
-# Define a function to get the frequency of a note
-def get_frequency(note, key):
-  # Get the frequency of the A note in the specified key
-  a_frequency = 440 * 2 ** ((key - 69) / 12)
-
-  # Get the frequency of the specified note
-  frequency = a_frequency * 2 ** ((note - 69) / 12)
-
-  return frequency
-
-# Create a title for the app
+# Create the Streamlit app
 st.title("Chord Progression Generator")
 
-# Create a text input for the key
-key = st.text_input("Key (C, Dm, Em, F, G, Am, Bb)")
-
-# Create a slider for the number of chords
-num_chords = st.slider("Number of Chords", 1, 10)
+# Get the user input
+key = st.selectbox("Key", ["C", "Dm", "Em", "F"])
+scale = st.selectbox("Scale", ["Major", "Minor"])
 
 # Generate a random chord progression
-progression = generate_chord_progression(key, num_chords)
-
-# Display the chord progression
-st.write("Chord Progression:", progression)
+progression = generate_chord_progression()
 
 # Play the chord progression
-play_chord_progression(progression, key)
+st.audio(progression, format="wav")
+
+# Display the chord progression
+st.write("Chord progression:", progression)
